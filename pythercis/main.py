b457302fbd4335f8fdc2141ffde0a65b51a198f8
Marcus Baw <marcusbaw@gmail.com>
2018-03-28 15:56:36 +0100

    refacoring of headers and work on template example

--- pythercis/main.py
+++ pythercis/main.py
@@ -1,5 +1,5 @@
 import requests
-
+import urllib
 
 class Pythercis:
 
@@ -7,7 +7,7 @@ class Pythercis:
         self.baseurl = baseurl
         # create new session
         self.create_session(username, password)
-        self.defaultheaders = {
+        self.default_headers = {
             'Ehr-Session': self.session_id
             }
 
@@ -26,47 +26,49 @@ class Pythercis:
             }
         response = requests.post(self.baseurl + api_path, headers=headers, params=querystring)
         self.session_id = response.headers['Ehr-Session']
+        print(response.text)
         return response
 
     def delete_session(self):
         api_path = "/rest/v1/session"
-        headers = {
-            'Ehr-Session': self.session_id
-            }
+        headers = self.default_headers
         response = requests.delete(self.baseurl + api_path, headers=headers)
+        print(response.text)
         return response
 
     def list_templates(self):
-        api_path = "/rest/v1/template"
-        headers = {
-            'Ehr-Session': self.session_id
-            }
+        api_path = "/rest/v1/template/"
+        headers = self.default_headers
         response = requests.get(self.baseurl + api_path, headers=headers)
+        print(response.text)
         return response
 
     def upload_template(self, template_relative_path):
         files = {'file': open(template_relative_path, 'rb')}
-        api_path = "/rest/v1/template"
-        headers = {
-            'Ehr-Session': self.session_id,
-            'Content-Type': 'application/xml'
-            }
+        api_path = "/rest/v1/template/"
+        headers = self.default_headers
+        headers['Content-Type'] = 'application/xml'
         response = requests.post(self.baseurl + api_path, headers=headers, files=files)
+        print(response.text)
         return response
 
     def delete_template(self, template_id):
         # this HTTP verb doesn't seem to be implemented in EtherCIS??
-        api_path = "/rest/v1/template"
-        headers = {
-            'Ehr-Session': self.session_id,
-            }
+        api_path = "/rest/v1/template/"
+        headers = self.default_headers
         response = requests.post(self.baseurl + api_path, headers=headers)
+        print(response.text)
         return response
 
     def template_example(self, template_id):
-        api_path = "/rest/v1/template"
-        headers = {
-            'Ehr-Session': self.session_id,
-            }
-        response = requests.post(self.baseurl + api_path, headers=headers)
+        api_path = "/rest/v1/template/"
+        headers = self.default_headers
+        payload = urllib.parse.quote(template_id) + "/example"
+        print(payload)
+        params = {
+            "format": "FLAT",
+            "exampleFilter": "INPUT"
+        }
+        response = requests.get(self.baseurl + api_path + payload, headers=headers, params=params)
+        print(response.text)
         return response
